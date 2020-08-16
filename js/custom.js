549c7b789aa3d90d1163f0222ea9fada9d5afb8b
Alice-Xiong <alicexxx0516@gmail.com>
2020-08-16 15:25:07 -0700

    resume box fixed

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,25 @@
-document.getElementById('modalBtn').onclick = displayPopup;
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function displayPopup() {
-  var popup = document.getElementById("popup");
-  var frame = document.getElementById('popupFrame');
-  frame.src = "https://drive.google.com/file/d/1zvfWcD0v4nl3I05k6yOzyDaYEdtSlF47/preview";
-  popup.style.display = "block";
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
